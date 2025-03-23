0121023ac77ec266e150bdcba746dd9fd12aeddf
georafia <georafia1@gmail.com>
2025-03-23 18:07:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand()
+tg.MainButton.textColor = "#FFFFFF"
+tg.MaimButton.color = "#2cab37"
 
-let btn = document.querySelector('#click')
+let btn1 = document.querySelector('#btn1')
+let btn2 = document.querySelector('#btn2')
+let btn3 = document.querySelector('#btn3')
+let btn4 = document.querySelector('#btn4')
 
-btn.onclick = () => {
+btn1.onclick = () => {
     if (tg.MainButton.IsVisible) {
         tg.MainButton.hide()
     } else {
-        tg.MainButton.setText("rahh")
+        tg.MainButton.setText("You bought: Moscow -> Sankt-Peterburg")
+        item = "Moscow -> Sankt-Peterburg"
         tg.MainButton.show()
     }
 }
+
+btn2.onclick = () => {
+    if (tg.MainButton.IsVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("You bought: Rostov-On-Don -> Krasnodar")
+        item = "Rostov-On-Don -> Krasnodar"
+        tg.MainButton.show()
+    }
+}
+
+btn3.onclick = () => {
+    if (tg.MainButton.IsVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("You bought: kislovodsk -> krasnodar")
+        item = "kislovodsk -> krasnodar"
+        tg.MainButton.show()
+    }
+}
+
+btn4.onclick = () => {
+    if (tg.MainButton.IsVisible) {
+        tg.MainButton.hide()
+    } else {
+        tg.MainButton.setText("You bought: Volgograd -> Volgodonsk")
+        item = "Volgograd -> Volgodonsk"
+        tg.MainButton.show()
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData(item);
+})
+
+tg.expand()
